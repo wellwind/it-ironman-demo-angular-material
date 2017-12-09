1aed928e5da0cd7c761f72ad6e38d1c12e7e93ff
wellwind <wellwind@mail2000.com.tw>
2017-12-10 00:23:30 +0800

    使用fixedInViewport讓SideNav蓋過Toolbar

--- src/app/shared-material/shared-material.module.ts
+++ src/app/shared-material/shared-material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule, MatButtonModule, MatButtonToggleModule, MatRippleModule, MatSidenavModule } from '@angular/material';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatRippleModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
-  exports: [MatButtonModule, MatIconModule, MatButtonModule, MatButtonToggleModule, MatRippleModule, MatSidenavModule]
+  exports: [MatButtonModule, MatIconModule, MatButtonModule, MatButtonToggleModule, MatRippleModule, MatSidenavModule, MatToolbarModule]
 })
 export class SharedMaterialModule {}
