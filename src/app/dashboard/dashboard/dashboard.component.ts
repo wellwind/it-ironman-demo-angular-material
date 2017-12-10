0dd2630499105834046c4ed3b57cc6d9dcd7cdc0
wellwind <wellwind@mail2000.com.tw>
2017-12-10 12:45:33 +0800

    設定toolbar顯示在最上方

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -12,7 +12,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {}
 
   toggleSideNav(sideNav: MatSidenav) {
-    sideNav.toggle().then((result: MatDrawerToggleResult) => {
+    sideNav.toggle().then((result: any) => {
       console.log(result);
       console.log(`選單狀態：${result.type}`);
     });
