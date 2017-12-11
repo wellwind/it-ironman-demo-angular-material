1dcbbf896cb703a10d6f0c57ad58a527ee9dde95
wellwind <wellwind@mail2000.com.tw>
2017-12-11 23:32:41 +0800

    自訂optionalLabel內容

--- src/app/dashboard/survey/survey.component.ts
+++ src/app/dashboard/survey/survey.component.ts
@@ -1,10 +1,16 @@
 import { FormGroup, FormControl, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { MatStepperIntl } from '@angular/material';
+
+export class TwStepperIntl extends MatStepperIntl {
+  optionalLabel = '非必填';
+}
 
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.css']
+  styleUrls: ['./survey.component.css'],
+  providers: [{ provide: MatStepperIntl, useClass: TwStepperIntl }]
 })
 export class SurveyComponent implements OnInit {
   isLinear: boolean;
