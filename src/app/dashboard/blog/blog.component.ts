22daa2980b8960acc738c8d73e0e0de657df2c8b
wellwind <wellwind@mail2000.com.tw>
2017-12-27 21:56:27 +0800

    設定spinner直徑

--- src/app/dashboard/blog/blog.component.ts
+++ src/app/dashboard/blog/blog.component.ts
@@ -10,6 +10,7 @@ import { Observable } from 'rxjs/Observable';
 export class BlogComponent implements OnInit {
   progress = 60;
   strokeWidth = 1;
+  diameter = 100;
   posts$: Observable<any>;
 
   constructor(private httpClient: HttpClient) {}
