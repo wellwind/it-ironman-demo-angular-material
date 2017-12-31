2365741f8fa509aebb821205a9ce4e64fb73c066
wellwind <wellwind@mail2000.com.tw>
2017-12-31 15:48:53 +0800

    使用mat-tab-group的相關事件

--- src/app/dashboard/inbox/inbox.component.ts
+++ src/app/dashboard/inbox/inbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-inbox',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InboxComponent implements OnInit {
   tabIndex = 0;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  tabFocusChange($event: MatTabChangeEvent) {
+    console.log(`focus變更，index：${$event.index}`);
   }
 
+  tabSelectedIndexChange($event: number) {
+    console.log(`selectedIndex變更，index：${$event}`);
+  }
+
+  tabSelectedTabChange($event: MatTabChangeEvent) {
+    console.log(`selectedTab變更，index：${$event.index}`);
+  }
 }
