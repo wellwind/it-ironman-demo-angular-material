3ac8fa7d5b0ed1138ef9871ce7ab48d61eb4f530
wellwind <wellwind@mail2000.com.tw>
2018-01-01 00:52:43 +0800

    data table排序

--- src/app/dashboard/inbox/email-list/email-list.component.ts
+++ src/app/dashboard/inbox/email-list/email-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource, MatPaginator, PageEvent } from '@angular/material';
+import { MatTableDataSource, MatPaginator, PageEvent, MatSort, Sort } from '@angular/material';
 
 @Component({
   selector: 'app-email-list',
@@ -9,25 +9,54 @@ import { MatTableDataSource, MatPaginator, PageEvent } from '@angular/material';
 })
 export class EmailListComponent implements OnInit {
   @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild('sortTable') sortTable: MatSort;
+
+  currentPage: PageEvent;
+  currentSort: Sort;
   emailsDataSource = new MatTableDataSource<any>();
   totalCount: number;
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    this.getIssuees(0, 10);
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+    this.currentSort = {
+      active: '',
+      direction: ''
+    };
+    this.getIssuees();
 
     // 分頁切換時，重新取得資料
     this.paginator.page.subscribe((page: PageEvent) => {
-      this.getIssuees(page.pageIndex, page.pageSize);
+      this.currentPage = page;
+      this.getIssuees();
     });
   }
 
-  getIssuees(pageIndex, pageSize) {
+  changeSort(sortInfo: Sort) {
+    if (sortInfo.active === 'created_at') {
+      sortInfo.active = 'created';
+    }
+    this.currentSort = sortInfo;
+    this.getIssuees();
+  }
+
+  getIssuees() {
+    const baseUrl = 'https://api.github.com/search/issues?q=repo:angular/material2';
+    let targetUrl = `${baseUrl}&page=${this.currentPage.pageIndex + 1}&per_page=${this.currentPage.pageSize}`;
+    if (this.currentSort.direction) {
+      targetUrl = `${targetUrl}&&sort=${this.currentSort.active}&order=${this.currentSort.direction}`;
+    }
     this.httpClient
-      .get<any>(`https://api.github.com/search/issues?q=repo:angular/material2&page=${pageIndex + 1}&per_page=${pageSize}`)
+      .get<any>(targetUrl)
       .subscribe(data => {
         this.totalCount = data.total_count;
         this.emailsDataSource.data = data.items;
+        // 從後端進行排序時，不用指定sort
+        // this.emailsDataSource.sort = this.sortTable;
         // 從後端取得資料時，就不用指定data srouce的paginator了
         // this.emailsDataSource.paginator = this.paginator;
       });
