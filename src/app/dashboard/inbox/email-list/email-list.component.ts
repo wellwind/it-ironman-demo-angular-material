b4787a02fc3d70553c7c7cc51b47d83f2c842dc3
wellwind <wellwind@mail2000.com.tw>
2018-01-01 00:03:08 +0800

    使用mat-paginator對後端資料進行分頁

--- src/app/dashboard/inbox/email-list/email-list.component.ts
+++ src/app/dashboard/inbox/email-list/email-list.component.ts
@@ -14,11 +14,23 @@ export class EmailListComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    this.httpClient.get<any>('https://api.github.com/search/issues?q=repo:angular/material2&page=1').subscribe(data => {
-      this.totalCount = data.items.length;
-      this.emailsDataSource.data = data.items;
-      this.emailsDataSource.paginator = this.paginator;
+    this.getIssuees(0, 10);
+
+    // 分頁切換時，重新取得資料
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.getIssuees(page.pageIndex, page.pageSize);
     });
+  }
+
+  getIssuees(pageIndex, pageSize) {
+    this.httpClient
+      .get<any>(`https://api.github.com/search/issues?q=repo:angular/material2&page=${pageIndex + 1}&per_page=${pageSize}`)
+      .subscribe(data => {
+        this.totalCount = data.total_count;
+        this.emailsDataSource.data = data.items;
+        // 從後端取得資料時，就不用指定data srouce的paginator了
+        // this.emailsDataSource.paginator = this.paginator;
+      });
   }
 
   replay(emailRow) {
