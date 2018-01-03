da6bf4f3eaffa14430d805dcfb008c7edf3d36f5
wellwind <wellwind@mail2000.com.tw>
2018-01-03 20:52:42 +0800

    建立SurveyInput directive，並實作focus()方法

---
+++ src/app/dashboard/survey/survey-input.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SurveyInputDirective } from './survey-input.directive';
+
+describe('SurveyInputDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SurveyInputDirective();
+    expect(directive).toBeTruthy();
+  });
+});
