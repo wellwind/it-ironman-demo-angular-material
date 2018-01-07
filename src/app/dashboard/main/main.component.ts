0df32e32f562c5e061d741819a3cd52ffdd64c6e
wellwind <wellwind@mail2000.com.tw>
2018-01-07 16:55:35 +0800

    使用Portal功能動態切換template

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild, ViewChildren, QueryList, TemplateRef, ViewContainerRef } from '@angular/core';
 import { MatIconRegistry, MatRipple } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Portal, CdkPortal, TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-main',
@@ -9,10 +10,13 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class MainComponent implements OnInit {
   @ViewChild(MatRipple) ripple: MatRipple;
+  @ViewChildren(CdkPortal) templatPortals: QueryList<CdkPortal>;
+  @ViewChild('template') template3: TemplateRef<any>;
+  currentPortal: Portal<any>;
   displayFocusTrap = false;
   displayContent = 999;
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {}
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private viewContainerRef: ViewContainerRef) {}
 
   ngOnInit() {
     this.matIconRegistry.addSvgIconInNamespace(
@@ -36,5 +40,18 @@ export class MainComponent implements OnInit {
 
   clearRipple() {
     this.ripple.fadeOutAll();
+  }
+
+  changePortal1() {
+    this.currentPortal = this.templatPortals.first;
+  }
+
+  changePortal2() {
+    this.currentPortal = this.templatPortals.last;
+  }
+
+  changePortal3() {
+    // 使用TemplatePortal把一般的TemplateRef包裝起來
+    this.currentPortal = new TemplatePortal(this.template3, this.viewContainerRef);
   }
 }
