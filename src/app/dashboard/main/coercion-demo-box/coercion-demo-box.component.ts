1dc8f1fe2c7a87a754fdf679278d7224d8fb6954
wellwind <wellwind@mail2000.com.tw>
2018-01-11 20:56:58 +0800

    示範coerceNumberProperty

--- src/app/dashboard/main/coercion-demo-box/coercion-demo-box.component.ts
+++ src/app/dashboard/main/coercion-demo-box/coercion-demo-box.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-coercion-demo-box',
@@ -13,9 +13,21 @@ export class CoercionDemoBoxComponent implements OnInit {
   get display(): boolean {
     return this._display;
   }
+
   set display(value: boolean) {
     console.log(value);
     this._display = coerceBooleanProperty(value);
+  }
+
+  private _height: number;
+
+  @Input()
+  get height(): number {
+    return this._height;
+  }
+
+  set height(value: number) {
+    this._height = coerceNumberProperty(value);
   }
 
   constructor() {}
